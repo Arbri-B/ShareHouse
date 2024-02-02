3a23fe06c36efb799009a730b3305b83c749b8ac
Arbri Berberi <74304861+Arbri-B@users.noreply.github.com>
2024-02-02 19:18:00 +0100

    change

--- client/src/views/LoginPage.jsx
+++ client/src/views/LoginPage.jsx
@@ -7,6 +7,7 @@ function LoginPage() {
     return (
         <>
             <div className="d-flex justify-content-betweem">
+                <h1>Arbri</h1>
                 <Login />
                 <Register />
             </div>
