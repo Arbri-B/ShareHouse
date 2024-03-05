704b843e3b9d111b70bdcdd45e66bf116f65a1cf
Arbri Berberi <74304861+Arbri-B@users.noreply.github.com>
2024-03-05 21:40:38 +0100

    changes to update

--- server/server.js
+++ server/server.js
@@ -5,8 +5,14 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const multer = require('multer')
 const path = require('path')
+const port= 8000;
 
 require('dotenv').config();
+
+const server = app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+});
+
 
 app.use(express.json()); 
 app.use(express.urlencoded({
@@ -24,16 +30,19 @@ require('./routes/post.routes')(app);
 require('./routes/user.routes')(app);
 
 
-app.listen(8000, () => {
-    console.log("Listening at Port 8000")
-})
-
 const io = socket(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'http://localhost:5173',
         methods: ['GET', 'POST'],
         allowedHeaders: ['*'],
         credentials: true,
     }
 });
 
+io.on('connection', socket => {
+    console.log(socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
+})
